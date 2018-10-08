14d826d4b64d4963717c62d934992e82d8892ebe
曾佑森 <2356281422@qq.com>
2018-10-08 22:02:39 +0800

    normalize

--- home/util/path/path.join.resolve.js
+++ home/util/path/path.join.resolve.js
@@ -19,3 +19,23 @@ module.exports.join = function(){
 };
 // http://nodejs.cn/api/path.html#path_path_normalize_path
 console.log(path.normalize("/foo/bar/baz/asdf/quux/.."));
+
+
+/**
+ * path.resolve([...paths])
+ * 这个接口的说明有点啰嗦。你可以想象现在你在shell下面，从左到右运行一遍cd path命令，最终获取的绝对路径/文件名，就是这个接口所返回的结果了。
+ * 比如 path.resolve('/foo/bar', './baz') 可以看成下面命令的结果
+ * cd /foo/bar
+ * cd ./baz
+*/
+console.log(path.resolve('')); // C:/Users/ZYS13/Desktop/node/home/util/path
+
+console.log(path.resolve('.')); // C:/Users/ZYS13/Desktop/node/home/util/path
+
+console.log(path.resolve('/foo/bar','./baz')); // C:/foo/bar/baz
+
+console.log(path.resolve('/foo/bar','./baz/')); // C:/foo/bar/baz
+
+console.log(path.resolve('/foo/bar','./tmp/file/')); // C:/foo/bar/tmp/file
+
+console.log( path.resolve('www', 'js/upload', '../mod.js') ); // C:/Users/ZYS13/Desktop/node/home/util/path/www/js/mod.js
---
+++ home/util/path/path.parse.js
@@ -0,0 +1,7 @@
+/**
+ * 路径解析
+ * path.parse(path) 
+ * 
+ * http://nodejs.cn/api/path.html#path_path_normalize_path
+ * path.normalize()
+*/
