ef152df213226ae0b2ebc8728740356018fb56a1
btea <2356281422@qq.com>
2018-12-27 17:44:41 +0800

    response deal

--- zhuishu.js
+++ zhuishu.js
@@ -21,19 +21,7 @@ let book = {
             port: 80,
             path: '/book/fuzzy-search?query=' + name +'&start=0&limit=10',
             method: 'GET',
-        }, function(res){
-            res.setEncoding('utf8');
-            let data = '';
-            res.on('data', function(chunk){
-                data += chunk;
-            });
-            res.on('end', function(){
-                _self.response.writeHead(200,'ok',{
-                    'Content-Type': 'text/plain;charset=utf-8'
-                });
-                _self.response.end(data);
-            })
-        });
+        }, this.responseDeal);
         req.on('error',function(e){
             console.log('problem with request:' + e.message);
         })
@@ -51,19 +39,7 @@ let book = {
             port: 80,
             path: '/mix-atoc/' + id + '?view=chapters',
             method: 'GET'
-        }, function(res){
-            let data = '';
-            res.setEncoding('utf8');
-            res.on('data', function(chunk){
-                data += chunk;
-            });
-            res.on('end', function(){
-                _self.response.writeHead(200,'ok',{
-                    'Content-Type': 'text/plain;charset=utf-8'
-                });
-                _self.response.end(data);
-            })
-        });
+        }, this.responseDeal);
         chapter.on('error', function(e){
             console.log('error!', e.message);
         });
@@ -79,27 +55,28 @@ let book = {
             port: 80,
             path: '/chapter/' + link + '?k=2124b73d7e2e1945&t=1468223717',
             method: 'GET'  
-        }, function(res){
-            res.setEncoding('utf8');
-            res.on('data', function(chunk){
-                // console.log(chunk);
-                data += chunk;
-            });
-            res.on('end',function(){
-                _self.response.writeHead(200,'ok',{
-                    'Context-Type': 'text/plain;chaset=utf-8'
-                });
-                _self.response.end(data);
-            })
-        });
+        }, this.responseDeal);
         cont.on('error', function(e){
             console.log('error!', e.message);
         });
         cont.end();
     },
-    // 看书背景
+    // 处理每个请求的响应部分
+    responseDeal: function(res){
+        var data = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk){
+            data += chunk;
+        });
+        res.on('end',function(){
+            book.response.writeHead(200,'ok',{
+                'Context-Type': 'text/plain;chaset=utf-8'
+            });
+            book.response.end(data);
+        })
+    },
+    // 背景图片分类获取
     types: ['recommend','hot','new'],
-
     biliPicturesApi: function(type, num, size){
         // https://api.vc.bilibili.com/link_draw/v2/Doc/index?type=hot&page_num=0&page_size=45
         let data = '', _self = this;
@@ -113,18 +90,7 @@ let book = {
             // headers: {
             //     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
             // }
-        },function(res){
-            res.setEncoding('utf8');
-            res.on('data', function(chunk){
-                data += chunk;
-            });
-            res.on('end', function(){
-                _self.response.writeHead(200,'ok',{
-                    'Context-Type': 'text/plain;chaset=utf-8'
-                });
-                _self.response.end(data);
-            })
-        });
+        },this.responseDeal);
         items.on('error', function(e){
             console.log('error!', e.message);
         });
